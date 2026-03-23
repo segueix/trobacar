5c14663b29335efcf1a52c28117509517e4fef32
Segueix <danielmasdexaxars@gmail.com>
2026-03-23 10:47:09 +0100

    Remove unused Play Services location dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,9 +48,6 @@ dependencies {
     // Gson per serialització JSON
     implementation("com.google.code.gson:gson:2.10.1")
     
-    // Location services
-    implementation("com.google.android.gms:play-services-location:21.1.0")
-    
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
